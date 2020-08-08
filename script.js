8fdc8c1fbdea54cf0e1854b800333db08fc043af
Alex Solov <Alextxnk@users.noreply.github.com>
2020-08-08 23:55:48 +0300

    style

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 alert('Hello world!'); //выводится модальное окно с сообщением
 console.log('Hello friends!');
 
+const money = 1000;
+const income = 15000;
+const addExpenses = 300;
+const deposit = 500;
+const mission = 250;
+const period = 365;
 /* let agePerson = 18;
 console.log('agePerson: ', agePerson);*/
