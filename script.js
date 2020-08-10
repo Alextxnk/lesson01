8db10287dfadaec8fc00c22a0ea97e6996718181
Alex Solov <Alextxnk@users.noreply.github.com>
2020-08-10 18:23:38 +0300

    style

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 alert('Hello world!'); 
 console.log('Hello friends!');
 
-const money;
-const income;
-const addExpenses;
-const deposit;
-const mission;
-const period;
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
