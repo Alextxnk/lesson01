41847e77de0725503d2d188d987897daf7759316
Alex Solov <Alextxnk@users.noreply.github.com>
2020-08-17 22:21:38 +0300

    style

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const isNumber = function(n){
+    return !isNaN(parseFloat(n)) && isFinite(n)
+};
+
 const money = +prompt('Ваш месячный доход?', 150000);
 
 const  income = 'Фриланс';
@@ -12,8 +16,16 @@ const deposit = confirm('Есть ли у вас депозит в банке?')
 const mission = 500000;
 const period = 6;
 
-const showTypeof = function(data){
-    console.log(data, typeof(data));
+const start = function(){
+    money = prompt('Ваш месячный доход?', 150000);
+    do{
+        money = prompt('Ваш месячный доход?', 150000);
+    }
+    while (!isNumber(money))
+};
+
+const showTypeof = function(item){
+    console.log(typeof item);
 };
 showTypeof(money);
 showTypeof(income);
@@ -28,20 +40,41 @@ const getAddExpenses = function(){
 const adddExpenses = getAddExpenses();
 console.log(adddExpenses);
 
-const expenses1 = prompt('Введите обязательную статью расходов?');
+let expenses1, expenses2;
+
+let getExpensesMonth = function(){
+    let sum = 0;
+
+    for (let i = 0; i < 2; i++) {
+        if (i === 0) {
+            expenses1 = prompt('Введите обязательную статью расходов?', "Продукты");
+        } else if (i === 1) {
+            expenses2 = prompt('Введите обязательную статью расходов?', "Квартплата");
+        }
+
+        sum += +prompt('Во сколько это обойдется?', 30000);
+    }
+    console.log(sum);
+    return sum;
+};
+
+let expensesAmount = getExpensesMonth();
+console.log('Сумма всех обязательных расходов за месяц= ' + expensesAmount);
+
+/*const expenses1 = prompt('Введите обязательную статью расходов?');
 const amount1 = +prompt('Во сколько это обойдется?', 40000);
 const expenses2 = prompt('Введите обязательную статью расходов?');
-const amount2 = +prompt('Во сколько это обойдется?', 20000);
+const amount2 = +prompt('Во сколько это обойдется?', 20000);*/
 
-const getExpensesMonth = function (){
+/*const getExpensesMonth = function (){
     return amount1 + amount2;
 };
 
 const expensesMonth = getExpensesMonth();
-console.log('Сумма всех обязательных расходов за месяц =', expensesMonth);
+console.log('Сумма всех обязательных расходов за месяц =', expensesMonth);*/
 
 const getAccumulatedMonth = function (){
-    return money - expensesMonth;
+    return money - expensesAmount;
 };
 
 const accumulatedMonth = getAccumulatedMonth();
